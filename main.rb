8b55448b5792afc78318bcc8604ff5c686f0b2c7
Aleksandr Scherbachenko <ascherbachenko@cs-cart.com>
2019-02-05 16:03:42 +0400

    Hand points amount calculation logic implemented.

--- main.rb
+++ main.rb
@@ -12,14 +12,14 @@ class Deck
   end
 
   def random_card
-    face = nil
-    face = @deck[suite].sample until face
-    @card_class.new(suite, face)
+    nominal = nil
+    nominal = @deck[suite].sample until nominal
+    @card_class.new(suite, nominal)
   end
 
   def random_card!
     card = random_card
-    @deck[card.suite].delete(card.face)
+    @deck[card.suite].delete(card.nominal)
     card
   end
 
@@ -33,26 +33,76 @@ end
 class Card
   SUITE_SYMBOLS = { spades: "\u2660", diamonds: "\u2666", hearts: "\u2665", clubs: "\u2663" }.freeze
 
-  attr_reader :suite, :face
+  attr_reader :suite, :nominal
 
-  def initialize(suite, face)
+  def initialize(suite, nominal)
     @suite = suite
-    @face = face
+    @nominal = nominal
   end
 
-  def value
+  def ace?
+    @nominal == :A
+  end
 
+  def face?
+    [:J, :Q, :K].include?(@nominal)
   end
 
   def to_s
-    print "#{@face}#{SUITE_SYMBOLS[@suite].encode("utf-8")}"
+    print "#{@nominal}#{SUITE_SYMBOLS[@suite].encode("utf-8")}"
   end
 end
 
-deck = Deck.new(Card)
-card = deck.random_card!
-puts card.to_s
-card = deck.random_card!
-puts card.to_s
+class Hand
+  ACE_MIN_AMOUNT = 1
+  ACE_MAX_AMOUNT = 11
+  FACE_CARD_AMOUNT = 10
+  BLACKJACK_AMOUNT = 21
 
-puts deck.deck
+  def initialize(*cards)
+    @cards = cards
+  end
+
+  def add_card(card)
+    @cards << card
+  end
+
+  def points_amount
+    initial, aces_last = initial_amount_and_hand_with_aces_last
+    aces_last.reduce(initial, &method(:add_card_amount_to_points_amount))
+  end
+
+  protected
+
+  def add_card_amount_to_points_amount(amount, card)
+    if card.ace?
+      return amount + ACE_MAX_AMOUNT > BLACKJACK_AMOUNT ? amount + ACE_MIN_AMOUNT : amount + ACE_MAX_AMOUNT
+    end
+
+    card.face? ? amount + FACE_CARD_AMOUNT : amount + card.nominal
+  end
+
+  def initial_amount_and_hand_with_aces_last
+    if aces.count == 2
+      return [1, not_aces << aces.first]
+    end
+
+    [0, not_aces + aces]
+  end
+
+  def aces
+    @cards.select { |card| card.ace? }
+  end
+
+  def not_aces
+    @cards.select { |card| !card.ace? }
+  end
+end
+
+card1 = Card.new(:spades, :A)
+card2 = Card.new(:spades, :A)
+card3 = Card.new(:spades, 9)
+
+puts card1.to_s, card2.to_s, card3.to_s
+hand = Hand.new(card1, card2, card3)
+print hand.points_amount
